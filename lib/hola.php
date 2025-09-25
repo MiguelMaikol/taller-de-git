31d2701559493d8acb79b16afab8508dfbfcabfd
MiguelDAW <miguel.pergom@educa.jcyl.es>
2025-09-25 13:20:01 +0200

    modificado archivo hola en master

--- lib/hola.php
+++ lib/hola.php
@@ -2,5 +2,10 @@
 // Autor: Miguel Perez>
 //hola oawlodlldwdwd
 print "Introduce tu nombre:";
+print "Introduce tu nombre:";
+print "Introduce tu nombre:";
+
+print "Introduce tu nombre:";
+print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
